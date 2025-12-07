9cc46d514fbc1ef2f7a3a2f3ec33e402a7cdbe08
Harry Sharma <harrysharma1066@gmail.com>
2025-12-07 17:08:26 +0000

    update: inbook bibtex formatting

--- helper/inbook.go
+++ helper/inbook.go
@@ -20,9 +20,9 @@ func FormatInbookBibtex(inbookCiteKey string, inbookAuthors []string, inbookTitl
 	// author
 	if len(inbookAuthors) > 0 {
 		if braces {
-			ret_string += fmt.Sprintf("	author       = %s", braces_open)
+			ret_string += fmt.Sprintf("	author    = %s", braces_open)
 		} else {
-			ret_string += fmt.Sprintf("	author       = %s", speechmarks)
+			ret_string += fmt.Sprintf("	author    = %s", speechmarks)
 		}
 		for i, inbookAuthor := range inbookAuthors {
 			ret_string += inbookAuthor
@@ -37,54 +37,54 @@ func FormatInbookBibtex(inbookCiteKey string, inbookAuthors []string, inbookTitl
 		}
 	} else {
 		if braces {
-			ret_string += fmt.Sprintf("	author       = %s<Lastname, Firstname>%s,\n", braces_open, braces_close)
+			ret_string += fmt.Sprintf("	author    = %s<Lastname, Firstname>%s,\n", braces_open, braces_close)
 		} else {
-			ret_string += fmt.Sprintf("	author       = %s<Lastname, Firstname>%s,\n", speechmarks, speechmarks)
+			ret_string += fmt.Sprintf("	author    = %s<Lastname, Firstname>%s,\n", speechmarks, speechmarks)
 		}
 	}
 
 	// title
 	if inbookTitle != "" {
 		if braces {
-			ret_string += fmt.Sprintf("	title        = %s%s%s,\n", braces_open, inbookTitle, braces_close)
+			ret_string += fmt.Sprintf("	title     = %s%s%s,\n", braces_open, inbookTitle, braces_close)
 		} else {
-			ret_string += fmt.Sprintf("	title        = %s%s%s,\n", speechmarks, inbookTitle, speechmarks)
+			ret_string += fmt.Sprintf("	title     = %s%s%s,\n", speechmarks, inbookTitle, speechmarks)
 		}
 	} else {
 		if braces {
-			ret_string += fmt.Sprintf("	title        = %s<Example Title: Please Change>%s,\n", braces_open, braces_close)
+			ret_string += fmt.Sprintf("	title     = %s<Example Title: Please Change>%s,\n", braces_open, braces_close)
 		} else {
-			ret_string += fmt.Sprintf("	title        = %s<Example Title: Please Change>%s,\n", speechmarks, speechmarks)
+			ret_string += fmt.Sprintf("	title     = %s<Example Title: Please Change>%s,\n", speechmarks, speechmarks)
 		}
 	}
 
 	// booktitle
 	if inbookBookTitle != "" {
 		if braces {
-			ret_string += fmt.Sprintf("	booktitle        = %s%s%s,\n", braces_open, inbookBookTitle, braces_close)
+			ret_string += fmt.Sprintf("	booktitle = %s%s%s,\n", braces_open, inbookBookTitle, braces_close)
 		} else {
-			ret_string += fmt.Sprintf("	booktitle        = %s%s%s,\n", speechmarks, inbookBookTitle, speechmarks)
+			ret_string += fmt.Sprintf("	booktitle = %s%s%s,\n", speechmarks, inbookBookTitle, speechmarks)
 		}
 	} else {
 		if braces {
-			ret_string += fmt.Sprintf("	booktitle        = %s<Example Title: Please Change>%s,\n", braces_open, braces_close)
+			ret_string += fmt.Sprintf("	booktitle = %s<Example Title: Please Change>%s,\n", braces_open, braces_close)
 		} else {
-			ret_string += fmt.Sprintf("	booktitle        = %s<Example Title: Please Change>%s,\n", speechmarks, speechmarks)
+			ret_string += fmt.Sprintf("	booktitle = %s<Example Title: Please Change>%s,\n", speechmarks, speechmarks)
 		}
 	}
 
 	// year
 	if inbookYear != "" {
 		if braces {
-			ret_string += fmt.Sprintf("	year         = %s%s%s\n", braces_open, inbookYear, braces_close)
+			ret_string += fmt.Sprintf("	year      = %s%s%s\n", braces_open, inbookYear, braces_close)
 		} else {
-			ret_string += fmt.Sprintf("	year         = %s%s%s\n", speechmarks, inbookYear, speechmarks)
+			ret_string += fmt.Sprintf("	year      = %s%s%s\n", speechmarks, inbookYear, speechmarks)
 		}
 	} else {
 		if braces {
-			ret_string += fmt.Sprintf("	year         = %s<2002: Please Change>%s\n", braces_open, braces_close)
+			ret_string += fmt.Sprintf("	year      = %s<2002: Please Change>%s\n", braces_open, braces_close)
 		} else {
-			ret_string += fmt.Sprintf("	year         = %s<2002: Please Change>%s\n", speechmarks, speechmarks)
+			ret_string += fmt.Sprintf("	year      = %s<2002: Please Change>%s\n", speechmarks, speechmarks)
 		}
 	}
 
@@ -106,30 +106,30 @@ func FormatInbookBibtex(inbookCiteKey string, inbookAuthors []string, inbookTitl
 	// address
 	if inbookAddress != "" {
 		if braces {
-			ret_string += fmt.Sprintf("	address = %s%s%s,\n", braces_open, inbookAddress, braces_close)
+			ret_string += fmt.Sprintf("	address   = %s%s%s,\n", braces_open, inbookAddress, braces_close)
 		} else {
-			ret_string += fmt.Sprintf("	address = %s%s%s,\n", speechmarks, inbookAddress, speechmarks)
+			ret_string += fmt.Sprintf("	address   = %s%s%s,\n", speechmarks, inbookAddress, speechmarks)
 		}
 	} else {
 		if braces {
-			ret_string += fmt.Sprintf("	address = %s<Example How Published: Please Change>%s,\n", braces_open, braces_close)
+			ret_string += fmt.Sprintf("	address   = %s<Example How Published: Please Change>%s,\n", braces_open, braces_close)
 		} else {
-			ret_string += fmt.Sprintf("	address = %s<Example How Published: Please Change>%s,\n", speechmarks, speechmarks)
+			ret_string += fmt.Sprintf("	address   = %s<Example How Published: Please Change>%s,\n", speechmarks, speechmarks)
 		}
 	}
 
 	// pages
 	if inbookPages != "" {
 		if braces {
-			ret_string += fmt.Sprintf("	pages = %s%s%s,\n", braces_open, inbookPages, braces_close)
+			ret_string += fmt.Sprintf("	pages     = %s%s%s,\n", braces_open, inbookPages, braces_close)
 		} else {
-			ret_string += fmt.Sprintf("	pages = %s%s%s,\n", speechmarks, inbookPages, speechmarks)
+			ret_string += fmt.Sprintf("	pages     = %s%s%s,\n", speechmarks, inbookPages, speechmarks)
 		}
 	} else {
 		if braces {
-			ret_string += fmt.Sprintf("	pages = %s<Example How Published: Please Change>%s,\n", braces_open, braces_close)
+			ret_string += fmt.Sprintf("	pages     = %s<Example How Published: Please Change>%s,\n", braces_open, braces_close)
 		} else {
-			ret_string += fmt.Sprintf("	pages = %s<Example How Published: Please Change>%s,\n", speechmarks, speechmarks)
+			ret_string += fmt.Sprintf("	pages     = %s<Example How Published: Please Change>%s,\n", speechmarks, speechmarks)
 		}
 	}
 	ret_string += "}"
